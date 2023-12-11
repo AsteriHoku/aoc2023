5f83354339c69d760870eff7cfa38ab1e0ba7aea
Sarah <sarah.matta@enformion.com>
2023-12-11 16:36:45 -0500

    day 04 part 2 solved

--- days/04/aoc-day-04.js
+++ days/04/aoc-day-04.js
@@ -1,27 +1,33 @@
 const fs = require('fs');
 let lines = fs.readFileSync('input-04.txt').toString().split("\n");
-let score = 0;
-//set up recursion so once we find cardScore we can compute for the 
-//appropriate number of next cards before then moving on to the next line
+let lineCounts = new Array(lines.length).fill(1);
 
-lines.forEach(line => {
-    let nextCount = 0;
-    nextCount = processCard(line);
-});
-
-function processCard() {
-    let cardScore = 0;
-    let numbers = line.match(/\b\d+\b(?!:)/g);
-    const duplicates = numbers.filter((item, index, arr) => arr.indexOf(item) !== index);
-    
-    for (let x = 0; x < duplicates.length; ++x) {
-        cardScore = cardScore === 0 ? 1 : cardScore * 2;
+console.log(`Congratulations you've reached the end and the answer is ${Solve(lines, lineCounts)}`);
+function Solve(lines, lineCounts){
+    for (let x = 0; x < lines.length; ++x){
+        for (let y = 0; y < lineCounts[x]; ++y){
+            let nextCount = processCard(lines[x]);
+            
+            for (let z = 1; z < nextCount+1; ++z){
+                lineCounts[x+z]++;
+            }
+        }
     }
 
-    return cardScore;
+    return lineCounts.reduce(function (a,b) {
+        return a+b;
+    }, 0);
+}
+function processCard(line) {
+    let numbers = line.match(/\b\d+\b(?!:)/g);
+    const duplicates = numbers.filter((item, index, arr) => arr.indexOf(item) !== index);
+
+    return duplicates.length;
 }
 
-//part
+
+
+//part 1
 // const fs = require('fs');
 // let lines = fs.readFileSync('input-04.txt').toString().split("\n");
 // let score = 0;
